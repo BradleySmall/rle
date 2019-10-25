f0a038660b5691a024e9db97d9223d7aa2c3461e
Bradley M. Small <bradley_small@hotmail.com>
2019-10-24 20:41:24 -0400

    adding generator version for rle/rld

---
+++ rle_generators.py
@@ -0,0 +1,48 @@
+"""Example using generators for run length encode/decode."""
+
+
+def rleunits(str1):
+    """Generate run length encoded units."""
+    current = None
+    count = 0
+
+    for letter in str1:
+        if letter == current:
+            count += 1
+        else:
+            if current:
+                yield str(count)+current
+            count = 1
+            current = letter
+
+
+def rldunits(str1):
+    """Generate run length decoded units."""
+    count = None
+
+    for c in str1:
+        if c.isdigit():
+            if not count:
+                count = c
+            else:
+                count += c
+        else:
+            yield(c * int(count))
+            count = None
+
+
+def main():
+    """Drive the example."""
+    str1 = "AAAABBBBCDDEEFFFFFFFFFFF"
+    encoded = ""
+    for i in rleunits(str1):
+        encoded += i
+    print(encoded)
+
+    unencoded = ""
+    for i in rldunits(encoded):
+        unencoded += i
+    print(unencoded)
+
+
+main()
